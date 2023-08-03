92988163c0a6a8979d61d057fd881b8ae055a89b
saiashish8364 <polusaiashish97@gmail.com>
2023-08-03 22:00:31 +0530

    up1

---
+++ meet-up/pages/index.js
@@ -0,0 +1,25 @@
+import MeetupList from "@/components/meetups/MeetupList";
+
+const dummy = [
+  {
+    id: "m1",
+    title: "a first meetup",
+    image:
+      "https://images.unsplash.com/photo-1587135941948-670b381f08ce?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    address: "taj",
+    description: "this is meet 1",
+  },
+  {
+    id: "m2",
+    title: "a second meetup",
+    image:
+      "https://plus.unsplash.com/premium_photo-1676464844981-09762ef89491?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=900&q=80",
+    address: "mountain",
+    description: "this is 2nd meetup",
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={dummy} />;
+}
+export default HomePage;
---
+++ meet-up/pages/new-meetup/index.js
@@ -0,0 +1,8 @@
+import NewMeetupForm from "@/components/meetups/NewMeetupForm";
+function NewMeetUpPage() {
+  function addMeetupHandler(enteredMeetupData) {
+    console.log(enteredMeetupData);
+  }
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+}
+export default NewMeetUpPage;
